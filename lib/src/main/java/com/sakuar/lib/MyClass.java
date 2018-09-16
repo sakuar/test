49c9bbaa606e2125c3dbd82595f9803ddc9df19e
sakuar <312807966@qq.com>
2018-09-16 22:13:11 +0800

    commit

---
+++ lib/src/main/java/com/sakuar/lib/MyClass.java
@@ -0,0 +1,14 @@
+package com.sakuar.lib;
+
+public class MyClass {
+
+
+    public static void main(String args[])
+    {
+        int y=0,x=2;
+
+        System.out.print("y="+y+",x="+x);
+    }
+
+
+}
